25c26eb187701344afca1e3b4878432a33d47031
xaf <xavier.mabras@protonmail.ch>
2017-01-30 15:59:14 +0100

    PSR1 recommends that each class must be in a namespace of at least one level to avoid collisions.

--- tests/CupsTest.php
+++ tests/CupsTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests;
+
 use CupsGenerator\Cups;
 
 /**
@@ -7,7 +9,7 @@ use CupsGenerator\Cups;
  *
  * @link https://es.wikipedia.org/wiki/C%C3%B3digo_Unificado_de_Punto_de_Suministro
  */
-class CupsTest extends PHPUnit_Framework_TestCase
+class CupsTest extends \PHPUnit_Framework_TestCase
 {
     public function testHasNormalLength()
     {
